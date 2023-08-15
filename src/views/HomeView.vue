2ee1f96c026147bc9af45743062643635211d2de
Hugo REY <hrey222@gmail.com>
2023-08-15 17:28:45 +0200

    hotfix: hide edit btn if not connected

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home flexCol">
     <div class="topRightIcon">
-      <svg @click="store.toggleEditMode()"
+      <svg @click="store.toggleEditMode()" v-if="auth.IsConnected">
         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="-3 -3 30 30" stroke-width="1.5" stroke="black" :class="{'scale':true,'active':store.editMode}">
         <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
       </svg>
